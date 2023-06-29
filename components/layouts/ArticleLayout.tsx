c1d700757780ccfd0d094e1986f46b538ae505ab
Claudio Lau <claudio.lau12@gmail.com>
2023-06-28 22:59:35 -0400

    summary section

--- components/layouts/ArticleLayout.tsx
+++ components/layouts/ArticleLayout.tsx
@@ -2,7 +2,7 @@ import { FC, PropsWithChildren } from 'react'
 
 export const ArticleLayout: FC<PropsWithChildren> = ({ children }) => {
     return (
-        <main className="mt-8 flex min-w-0 flex-auto flex-col bg-black px-2 md:px-0">
+        <main className="mt-8 flex min-w-0 flex-auto flex-col px-2 md:px-0">
             {children}
         </main>
     )
