68d7315af3c2921a835765e04b1da7578909d025
Claudio Lau <claudio.lau12@gmail.com>
2023-07-15 13:08:31 -0400

    update

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,14 +1,12 @@
+import { NextResponse } from 'next/server'
 import github from './github.json'
-import type { NextApiResponse } from 'next'
 
-const fetchPublicRepositories = github
-
-export async function GET(res: NextApiResponse) {
+export async function GET() {
     try {
         const username = process.env.NEXT_PUBLIC_GithubOwner
         if (!username) throw new Error('Environment name not found')
 
-        res.status(200).send(fetchPublicRepositories)
+        return NextResponse.json(github)
     } catch (error) {
         console.error(error)
     }
