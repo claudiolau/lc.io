8a04788692c79bea8cac8312ce925fe478985f98
Claudio Lau <claudio.lau12@gmail.com>
2023-07-15 12:42:39 -0400

    added cors

--- next.config.js
+++ next.config.js
@@ -1,35 +1,27 @@
 /** @type {import('next').NextConfig} */
 
-const getCorsHeaders = () => {
-    const headers = {}
-
-    headers['Access-Control-Allow-Origin'] = 'https://localhost:3000'
-    headers['Access-Control-Allow-Credentials'] = 'true'
-    headers['Access-Control-Allow-Methods'] =
-        'GET,OPTIONS,PATCH,DELETE,POST,PUT'
-    headers['Access-Control-Allow-Headers'] =
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
-
-    return Object.entries(headers).map(([key, value]) => ({ key, value }))
-}
-
 const nextConfig = {
     images: {
         domains: ['opengraph.githubassets.com'],
     },
-    headers: async () => {
+
+    async headers() {
         return [
             {
-                source: '/api/(.*)',
-                headers: getCorsHeaders(),
-            },
-        ]
-    },
-    async rewrites() {
-        return [
-            {
+                // matching all API routes
                 source: '/api/:path*',
-                destination: 'http://localhost:3000/:path*',
+                headers: [
+                    { key: 'Access-Control-Allow-Credentials', value: 'true' },
+                    { key: 'Access-Control-Allow-Origin', value: '*' },
+                    {
+                        key: 'Access-Control-Allow-Methods',
+                        value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+                    },
+                    {
+                        key: 'Access-Control-Allow-Headers',
+                        value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+                    },
+                ],
             },
         ]
     },
