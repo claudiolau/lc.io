08adeb9f22a74b72415116aa41190f83a8db3215
Claudio Lau <claudio.lau12@gmail.com>
2023-07-15 12:40:36 -0400

    added cors

--- next.config.js
+++ next.config.js
@@ -25,6 +25,14 @@ const nextConfig = {
             },
         ]
     },
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:3000/:path*',
+            },
+        ]
+    },
 }
 
 module.exports = nextConfig
