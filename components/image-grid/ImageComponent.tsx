f62bcbc037c4b06c0cd9ed7de6d985f4f0015550
Claudio Lau <claudio.lau12@gmail.com>
2023-07-15 10:20:23 -0400

    fixed aspect ratio of images

--- components/image-grid/ImageComponent.tsx
+++ components/image-grid/ImageComponent.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 type Size = {
     width: number
@@ -10,20 +10,21 @@ export default function BlurImage({
     width,
     height,
 }: Size & { image: string }) {
-    const [isLoading, setLoading] = useState(true)
+    const [isLoading, setLoading] = useState(false)
     const classNameBlur = `duration-700 ease-in-out group-hover:opacity-75
     ${
         isLoading
-            ? 'scale-110 blur-2xl grayscale'
-            : 'scale-100 blur-0 grayscale-0'
+            ? 'scale-100 blur-0 grayscale-0'
+            : 'scale-110 blur-2xl grayscale'
     })`
+
     return (
-        <div className="w-full overflow-hidden rounded-lg bg-gray-200">
+        <div className="w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-9 xl:aspect-w-7">
             <Image
                 alt=""
                 src={image}
                 className={classNameBlur}
-                onLoadingComplete={() => setLoading(false)}
+                onLoadingComplete={() => setLoading(true)}
                 width={width}
                 height={height}
             />
@@ -32,7 +33,7 @@ export default function BlurImage({
 }
 export const ImageComponent = ({ width, height }: Size) => {
     return (
-        <div className="my-8 grid columns-2 gap-4 sm:grid-cols-3 sm:gap-x-8">
+        <div className="my-8 grid grid-cols-3 gap-4 sm:grid-cols-3 sm:gap-x-8">
             <BlurImage image={'/1.jpeg'} width={width} height={height} />
             <BlurImage image={'/2.jpeg'} width={width} height={height} />
             <BlurImage image={'/3.jpeg'} width={width} height={height} />
