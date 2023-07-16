b84db51ccd9ab5dd16c5d522c290e536775bd825
Claudio Lau <claudio.lau12@gmail.com>
2023-07-16 16:53:40 -0400

    fix linting

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,5 +1,5 @@
-import { NextResponse } from 'next/server'
-import github from './github.json'
+// import { NextResponse } from 'next/server'
+// import github from './github.json'
 
 export async function GET() {
     try {
