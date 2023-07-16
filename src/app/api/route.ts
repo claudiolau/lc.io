944da10b6c10412b82c74a88a9de90bd1d2de91b
Claudio Lau <claudio.lau12@gmail.com>
2023-07-16 17:03:38 -0400

    testing

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,24 +1,24 @@
-// import { NextResponse } from 'next/server'
-// import github from './github.json'
+import { NextResponse } from 'next/server'
+import github from './github.json'
 
 export async function GET() {
     try {
-        // const augmentData = github.map((x) => ({
-        //     name: x.name,
-        //     description: x.description,
-        //     html_url: x.html_url,
-        // }))
+        const augmentData = github.map((x) => ({
+            name: x.name,
+            description: x.description,
+            html_url: x.html_url,
+        }))
 
-        // return NextResponse.json(augmentData)
-        return new Response('Hello, Next.js!', {
-            status: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                'Access-Control-Allow-Methods':
-                    'GET, POST, PUT, DELETE, OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-            },
-        })
+        return NextResponse.json(augmentData)
+        // return new Response('Hello, Next.js!', {
+        //     status: 200,
+        //     headers: {
+        //         'Access-Control-Allow-Origin': '*',
+        //         'Access-Control-Allow-Methods':
+        //             'GET, POST, PUT, DELETE, OPTIONS',
+        //         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        //     },
+        // })
     } catch (error) {
         console.error(error)
     }
